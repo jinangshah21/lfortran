88eb87f5cb1e7e048d66275bc7cbfb4b9b66b771
jinangshah21 <jinang2110@gmail.com>
2025-04-29 23:32:14 +0530

    minors

--- integration_tests/formatted_read_1.f90
+++ integration_tests/formatted_read_1.f90
@@ -1,7 +1,7 @@
 program test_stdin
     use iso_c_binding
     implicit none
-    character(len=19) :: a
+    character(len=20) :: a
     integer :: istty
     logical :: is_gfortran = .false.
 
@@ -21,13 +21,17 @@ program test_stdin
 
     if (istty == 1) then
         if (is_gfortran) then
-            call execute_command_line("gfortran formatted_read_1.f90 && ./a.out < formatted_read_1.f90")
+            call execute_command_line("gfortran ./integration_tests/formatted_read_1.f90 &&" // &
+            "./a.out < ./integration_tests/formatted_read_1.f90")
         else
-            call execute_command_line("./src/bin/lfortran --cpp formatted_read_1.f90 < formatted_read_1.f90")
+            call execute_command_line("./src/bin/lfortran --cpp ./integration_tests/formatted_read_1.f90 " // &
+            "< ./integration_tests/formatted_read_1.f90")
         end if
+        stop
     else
         ! If not interactive, read from stdin
         read(*, '(a)') a
         print *, "From stdin:", trim(a)
+        if (a /= "program test_stdin") error stop
     end if
 end program
